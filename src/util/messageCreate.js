16249ee84ce41fd5cf73da5a9af3aa18f29a6987
adryd <me@adryd.com>
2021-08-14 16:26:22 -0400

    Whoops! left some debug code in

--- src/util/messageCreate.js
+++ src/util/messageCreate.js
@@ -98,10 +98,6 @@ module.exports = async function handleMessage(message) {
     }
   }
 
-  await new Promise((resolve) => {
-    setTimeout(resolve, 5000);
-  });
-
   const syntaxTree = markdownParser(message.content);
 
   // Get all tweets from message, this starts fetching things in the background
