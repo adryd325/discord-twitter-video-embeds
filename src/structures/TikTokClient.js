a1495fdcd1b8adf0014648d517a5cbab9a04b013
adryd <me@adryd.com>
2022-03-15 23:41:48 -0400

    quote url

--- src/structures/TikTokClient.js
+++ src/structures/TikTokClient.js
@@ -5,7 +5,7 @@ class TikTokClient {
   async getPost(match) {
     const url = match[0];
     // This should be safe as our regexes earlier prevent any weirdness
-    return await sh(`yt-dlp ${url} -j`).then((stdout)=>{
+    return await sh(`yt-dlp "${url.replace(/"/g, '\"')}" -j`).then((stdout)=>{
       return new TikTokPost(JSON.parse(stdout))
     });
   }
