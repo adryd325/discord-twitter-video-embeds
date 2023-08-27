635dc024c7d7a5f946709b96141ff52f21854d5a
adryd <me@adryd.com>
2023-08-26 22:44:20 -0400

    asdf

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require("discord.js");
 
-const manager = new ShardingManager("./bot.js", { token: process.env.TOKEN });
+const manager = new ShardingManager("./src/bot.js", { token: process.env.TOKEN });
 
 manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
 
